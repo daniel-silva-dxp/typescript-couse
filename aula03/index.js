7805093a86621475c5e90a7bbe94648000ba041b
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-08 12:59:01 -0300

    feat: delete file

--- aula03/index.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.getElementById('input1');
-var input2 = document.getElementById('input2');
-var button = document.getElementById('soma');
-function somar(a, b) {
-    return a + b;
-}
-button.addEventListener('click', function () {
-    console.log(somar(Number(input1.value), Number(input2.value)));
-});
