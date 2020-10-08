59b0605629767285fababbbcd7fbee3fefe95e4e
Daniel Silva <dani3l.dxp@gmail.com>
2020-10-08 16:21:27 -0300

    feat: métodos de promises

--- JS/promises.js
+++ JS/promises.js
@@ -6,18 +6,31 @@ function rand(min, max) {
 
 function delay(msg, temp) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(msg);
-    }, temp);
+    try {
+      setTimeout(() => {
+        resolve(msg);
+      }, temp);
+    } catch (e) {
+      reject(e);
+    }
   });
 }
 
-delay("Frase I", rand(1, 5)).then((response) => {
-  console.log(response);
-  return delay("Frase II", rand(1, 2)).then((response) => {
-    console.log(response);
-    return delay("Fase III", rand(1, 1)).then((response) => {
-      console.log(response);
-    });
-  });
-});
+// delay("Frase I", rand(1, 5)).then((response) => {
+//   console.log(response);
+//   return delay("Frase II", rand(1, 2)).then((response) => {
+//     console.log(response);
+//     return delay("Fase III", rand(1, 1)).then((response) => {
+//       console.log(response);
+//     });
+//   });
+// });
+
+const arr = [
+  delay("Primeira Promise", rand(1, 5)),
+  delay("Segunda Promise", rand(1, 2)),
+  delay("Terceira Promise", rand(1, 3)),
+];
+
+// Promise.all(arr).then((response) => console.log(response));
+Promise.race(arr).then((response) => console.log(response));
