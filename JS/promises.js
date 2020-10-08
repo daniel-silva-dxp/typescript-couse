7af483ab82250b8b3004b303b668de8c6592994d
Daniel Silva <dani3l.dxp@gmail.com>
2020-10-08 16:34:22 -0300

    feat: async await

--- JS/promises.js
+++ JS/promises.js
@@ -26,11 +26,22 @@ function delay(msg, temp) {
 //   });
 // });
 
-const arr = [
-  delay("Primeira Promise", rand(1, 5)),
-  delay("Segunda Promise", rand(1, 2)),
-  delay("Terceira Promise", rand(1, 3)),
-];
+// const arr = [
+//   delay("Primeira Promise", rand(1, 5)),
+//   delay("Segunda Promise", rand(1, 2)),
+//   delay("Terceira Promise", rand(1, 3)),
+// ];
 
 // Promise.all(arr).then((response) => console.log(response));
-Promise.race(arr).then((response) => console.log(response));
+// Promise.race(arr).then((response) => console.log(response));
+
+async function play() {
+  const val1 = await delay("Primeira Promise", rand(1, 5));
+  console.log(val1);
+  const val2 = await delay("Segunda Promise", rand(1, 2));
+  console.log(val2);
+  const val3 = await delay("Terceira Promise", rand(1, 3));
+  console.log(val3);
+}
+
+play();
